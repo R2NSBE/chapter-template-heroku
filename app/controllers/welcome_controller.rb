64b2c4fb79790d79e8129a03b9d6a9dc118bd4e3
Keheira <keheirah@gmail.com>
2016-04-04 01:34:13 -0400

    Added basic ROR files

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def show
+    if valid_page?
+      render template: "welcome/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/welcome/#{params[:page]}.html.erb"))
+  end
+end
